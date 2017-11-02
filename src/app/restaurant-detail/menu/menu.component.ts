ae2127c11af1540549bea57c3ba75b5494c3d05c
guilhermeurnau <gaudstoo@hotmail.com>
2017-11-02 17:18:17 -0200

    Aula 51

--- src/app/restaurant-detail/menu/menu.component.ts
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MenuItem } from '../menu-item/menu-item.model'
+import { RestaurantsService } from '../../restaurants/restaurants.service'
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'mt-menu',
   templateUrl: './menu.component.html'
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  menuItens: Observable<MenuItem[]>
+
+  constructor(private restaurantService: RestaurantsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.menuItens = this.restaurantService.menuOfRestaurant(this.route.snapshot.parent.params['id']);
   }
 
+  addMenuItem(item: MenuItem){
+    
+  }
 }
