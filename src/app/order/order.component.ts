d76aaf81af3748f431aa188e415e139756656b37
guilhermeurnau <gaudstoo@hotmail.com>
2017-11-06 19:35:27 -0200

    Aula 70

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -3,6 +3,7 @@ import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from 'app/order/order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from 'app/order/order.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mt-order',
@@ -18,7 +19,8 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão Refeição', value: 'REF' }
   ]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -48,11 +50,8 @@ export class OrderComponent implements OnInit {
       .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order)
       .subscribe((orderId: string) => {
-        console.log(`Compra concluída: ${orderId}`)
+        this.router.navigate(['/order-summary'])
         this.orderService.clear()
       })
-
-    console.log(order)
   }
-
 }
